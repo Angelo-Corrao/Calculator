de83f5c57f4e1ddc9b8b961556bc41605ddf8fdf
Angelo <precipitevoliss1@gmail.com>
2022-03-03 20:03:35 +0100

    Linear Layout

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -6,15 +6,17 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    private var firstFactor: Float = 0f
-    private var secondFactor: Float = 0f
-    private var operationPressed: Boolean = false
-    private var operation: String = ""
+
+    private lateinit var binding : ActivityMainBinding
+    private var currentNumber = 0f
+    private var oldNumber = 0f
+    private var operation = ""
+    private var previousOperation = ""
+    private var isNewOperation = false
     private var outputNumber = "0"
-        get() = binding.resultTv.text.toString()
+        get() = binding.numberText.text.toString()
         set(value) {
-            binding.resultTv.text = value.toString()
+            binding.numberText.text = value
             field = value
         }
 
@@ -24,101 +26,135 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
     }
 
-    private fun onItemClick(view: View) {
-        if (operationPressed){
-
+    fun onItemClick(view: View){
+        if(isNewOperation){
+            oldNumber = currentNumber
+            outputNumber = ""
+            isNewOperation = false
         }
 
-        when (view.id){
-            R.id.number0Btn -> {
-                setNumber("0")
+        when(view.id){
+            R.id.cancelButton -> {
+                oldNumber = 0f
+                currentNumber = 0f
+                outputNumber = "0"
             }
-
-            R.id.number1Btn -> {
-                setNumber("1")
-            }
-
-            R.id.number2Btn -> {
-                setNumber("2")
-            }
-
-            R.id.number3Btn -> {
-                setNumber("3")
-            }
-
-            R.id.number4Btn -> {
-                setNumber("4")
-            }
-
-            R.id.number5Btn -> {
-                setNumber("5")
-            }
-
-            R.id.number6Btn -> {
-                setNumber("6")
-            }
-
-            R.id.number7Btn -> {
-                setNumber("7")
-            }
-
-            R.id.number8Btn -> {
-                setNumber("8")
-            }
-
-            R.id.number9Btn -> {
-                setNumber("9")
-            }
-
-            R.id.decimalBtn -> {
-
-            }
-
-            R.id.cancelBtn -> {
-
-            }
-
-            R.id.invertBtn -> {
-
-            }
-
-            R.id.percentageBtn -> {
-
-            }
-
-            R.id.divideBtn -> {
-
-            }
-
-            R.id.multiplicationBtn -> {
-
-            }
-
-            R.id.subtractionBtn -> {
-
-            }
-
-            R.id.additionBtn -> {
-                if (!operationPressed){
-                    operation = "+"
-                    firstFactor = outputNumber.toFloat()
+            R.id.invertButton -> {
+                if(!outputNumber.contains("-")) {
+                    currentNumber *= -1
+                    outputNumber = "-$outputNumber"
+                }
+                else{
+                    currentNumber *= -1
+                    outputNumber = outputNumber.drop(1)
                 }
             }
-
-            R.id.equalBtn -> {
-
+            R.id.percentageButton -> {
+                operation = "%"
+                total()
+                previousOperation = "%"
+            }
+            R.id.divide -> {
+                operation = "/"
+                total()
+                previousOperation = "/"
+            }
+            R.id.number7 -> {
+                setNumber(7)
+            }
+            R.id.number8 -> {
+                setNumber(8)
+            }
+            R.id.number9 -> {
+                setNumber(9)
+            }
+            R.id.multiply -> {
+                operation = "*"
+                total()
+                previousOperation = "*"
+            }
+            R.id.number4 -> {
+                setNumber(4)
+            }
+            R.id.number5 -> {
+                setNumber(5)
+            }
+            R.id.number6 -> {
+                setNumber(6)
+            }
+            R.id.subtract -> {
+                operation = "-"
+                total()
+                previousOperation = "-"
+            }
+            R.id.number1 -> {
+                setNumber(1)
+            }
+            R.id.number2 -> {
+                setNumber(2)
+            }
+            R.id.number3 -> {
+                setNumber(3)
+            }
+            R.id.add -> {
+                operation = "+"
+                total()
+                previousOperation = "+"
+            }
+            R.id.number0 -> {
+                setNumber(0)
+            }
+            R.id.pointButton -> {
+                if(!outputNumber.contains("."))
+                    outputNumber = "$outputNumber."
+            }
+            R.id.resultButton -> {
+                makeOperations(operation)
             }
         }
     }
 
-    private fun setNumber(value: String) {
-        if (outputNumber == "0" || outputNumber == "0.0")
-            outputNumber = value
-        else
-            outputNumber += value
+    private fun setNumber(number: Int){
+        if(outputNumber == "0" || outputNumber == "0.0"){
+            outputNumber = number.toString()
+        }
+        else{
+            outputNumber += number
+        }
+
+        currentNumber = outputNumber.toFloat()
     }
 
-    private fun calculateResult() {
+    private fun total() {
+        if(oldNumber != 0f && currentNumber != 0f){
+            makeOperations(previousOperation)
+        }
+        isNewOperation = true
+    }
 
+    private fun makeOperations(operation: String){
+        var finalResult = 0f
+        when(operation){
+            "+" -> {
+                finalResult = oldNumber + currentNumber
+            }
+
+            "-" -> {
+                finalResult = oldNumber - currentNumber
+            }
+
+            "*" -> {
+                finalResult = oldNumber * currentNumber
+            }
+
+            "/" -> {
+                finalResult = oldNumber / currentNumber
+            }
+            "%" -> {
+                finalResult = (oldNumber / 100) * currentNumber
+            }
+        }
+        currentNumber = finalResult
+        outputNumber = finalResult.toString()
     }
 }
